1ca746779ce9467477934954f56659b0b579f68a
ZHAOQI CHEN <z.chen5.21@abdn.ac.uk>
2022-03-13 22:10:15 +0000

    Heroku commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5$m0qu)am^2__b%-=b0dxxse*2ch-)yk*ps1plsy!$n)8x)89='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['damp-lowlands-76266.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['damp-lowlands-76266.herokuapp.com/', 'localhost']
 
 
 # Application definition
